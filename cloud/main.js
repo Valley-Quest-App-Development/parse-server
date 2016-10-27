e0b5b61665ac785a6a939d5ab941b3d91be8b4b7
John Kotz <john.lyme@mac.com>
2016-10-27 17:19:39 -0400

    Pushit push

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,89 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.afterSave('QuestGPSSet', function(req, res) {
+	var query = new Parse.Query("QuestGPSSet")
+
+	query.find({
+		success: function(results) {
+			var dictionary = {}; // In format: <String: {start: [], end: []}>
+			
+			for (i in results) {
+				var result = results[i]
+				var type = result.get("placeType");
+				var loc = result.get("point");
+				var quest = result.get("quest").id;
+
+				var val = dictionary[quest];
+
+				if (type == "start") {
+					if (val == undefined) {
+						dictionary[quest] = {start: [loc], end: []};
+					}else{
+						var start = val.start;
+						start.push(loc);
+						dictionary[quest] = {start: start, end: val.end};
+					}
+				}else{
+					if (val == undefined) {
+						dictionary[quest] = {start: [], end: [loc]};
+					}else{
+						var end = val.end;
+						end.push(loc);
+						dictionary[quest] = {start: val.start, end: end};
+					}
+				}
+			}
+
+			var left = Object.keys(dictionary).length
+			// Now I will remove that ones that don't have enough data and average the rest
+			for (questID in dictionary) {
+				var val = dictionary[questID];
+
+				if (val == undefined || val.start.length < 2 || val.end.length < 2) {
+					delete dictionary[questID];
+					left -= 1;
+					continue;
+				}
+
+				var totalLat = 0.0
+				var totalLong = 0.0
+
+				for (i in val.start) {
+					totalLat += val[i].latitude
+					totalLong += val[i].longitude
+				}
+
+				var startAvg = new Parse.GeoPoint(totalLat / val.start.length, totalLong / val.start.length)
+
+				totalLat = 0.0
+				totalLong = 0.0
+
+				for (i in val.end) {
+					totalLat += val[i].latitude
+					totalLong += val[i].longitude
+				}
+
+				var query = new Parse.Query("Quest");
+				query.get(quest, {
+					success: function(questObj) {
+						questObj.set("gps_loc", startAvg);
+						questObj.set("gps_end", {latitude: totalLat / val.end.length, longitude: totalLong / val.end.length})
+						questObj.save();
+
+						left -= 1;
+
+						if (left <= 0) {
+							res.success()
+						}
+					}
+				});
+
+				if (left <= 0) {
+					res.success()
+				}
+			}
+		}
+	});
+});
